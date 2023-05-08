35666d568790b93989d49f174d187a20d058a769
Adil9212 <abdrazakovadil@gmail.com>
2023-05-08 17:58:21 +0600

    less

---
+++ script.js
@@ -0,0 +1,40 @@
+// h\ m 1
+// const listit = document.querySelectorAll('li')
+// console.log(listit);
+
+//h \ m 2
+// const listit = document.querySelectorAll('li')
+// console.log(listit);
+
+//h \ m 3
+// const listit = document.querySelectorAll('.list-item')
+// console.log(listit);
+
+//h \ m  4
+// const lists = document.querySelectorAll('.list-item')
+// for (let i = 0; i > lists.length; i++) {
+//     console.log(lists[i].textContent);
+// }
+
+// h \ m 5
+// const listit = document.querySelector("[data-test-id]");
+// let arryy = []
+// for (let i = 0; i < listit.length; i++) {
+//     arryy.push(listit[i].getAttribute('data-test-id'))
+// }
+// console.log(arryy);
+
+//h \ m 6
+// const lists = document.querySelectorAll(".list-item");
+// for (let i = 0; i < lists.length; i++) {
+//     lists[i].className.includes(lists[i].textContent) === false ? console.log(lists[i]) : '';
+// }
+
+
+// h  \  m  7
+// const lists = document.querySelectorAll(".list-item");
+// let arr = []
+// for (let i = 0; i < lists.length; i++) {
+//     arr.push(lists[i].getAttribute('data-test-id'))
+//     lists[i].textContent = arr[i] + " " + lists[i].textContent
+// }
